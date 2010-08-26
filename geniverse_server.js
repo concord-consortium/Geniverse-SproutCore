bdc665f5c4b1fb8ce3843cef9166c05e5b59d695
Richard Klancer <rpk@pobox.com>
2010-08-26 19:03:47 -0400

    Updated geniverse server to proxy using node-http-proxy

--- geniverse_server.js
+++ geniverse_server.js
@@ -47,3 +47,27 @@ myApp.build(function() {
   server.run();
   
 });
+
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    sys = require('sys');
+
+// create a proxy server with custom application logic
+httpProxy.createServer(function (req, res, proxy) {
+  if (req.url.match(/^\/rails/)) {
+    sys.puts('proxying to rails: ', req.url);
+    proxy.proxyRequest(3000, 'localhost', req, res);
+  }
+  else if (req.url.match(/^\/geniverse\//) || req.url.match(/^\/chat\//)) {
+    sys.puts('proxying to geniverse.dev.concord.org: ', req.url);
+    req.headers.host = 'geniverse.dev.concord.org';
+    proxy.proxyRequest(80,'geniverse.dev.concord.org', req, res);
+  }
+  else {
+    sys.puts('proxying to garcon: ', req.url);
+    proxy.proxyRequest(8000, 'localhost', req, res);
+  }
+}).listen(9000);
+
+
+
