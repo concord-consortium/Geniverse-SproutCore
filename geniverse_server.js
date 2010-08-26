a8ff1701d16830ad9a00d9c8680c44fb4277b3b3
Richard Klancer <rpk@pobox.com>
2010-08-26 16:53:02 -0400

    garcon server script (not enough proxies yet though)

---
+++ geniverse_server.js
@@ -0,0 +1,49 @@
+var g = require('./garcon/lib/garçon'),
+    server, myApp;
+    
+// create a server which will listen on port 8000 by default
+server = new g.Server({proxyHost: 'localhost', proxyPort: 3000});
+// adding an application named 'myapp' tells the server to respond to
+// the /myapp url and to create a myapp.html file when saving
+myApp = server.addApp({
+  name: 'geniverse',
+  theme: 'sc-theme',
+  buildLanguage: 'english'
+});
+
+// myApp needs SproutCore to run
+myApp.addSproutcore();
+
+// add other dependencies
+myApp.addFrameworks(
+  { path: 'frameworks/cc/frameworks/cc_chat' },
+  { path: 'frameworks/cc/frameworks/cc' },
+  
+  // the theme you're using
+  { path:'frameworks/sproutcore/themes/standard_theme', combineScripts: true },
+  
+  // if you're on Quilmes and use Ace, uncomment the next 2 lines instead
+  // { path:'frameworks/sproutcore/themes/empty_theme', combineScripts: true },
+  // { path:'frameworks/sproutcore/themes/ace', combineScripts: true },
+  
+  // finally, the sources for myApp must be added as well
+  { path: 'apps/' + myApp.name }
+);
+
+// add some html for inside the <head> tag
+myApp.htmlHead = '<title>Geniverse</title>';
+
+// add some html for inside the <body> tag
+myApp.htmlBody = [
+  '<div id="sendButtonContainer" style="visibility: hidden;">button</div>',
+  '<script src="/geniverse/geniverse/geniverse.nocache.js" type="text/javascript"></script>',
+  '<script src="/chat/comet.js" type="text/javascript"></script>'
+].join('\n');
+
+// build the app
+myApp.build(function() {
+  
+  // run the server
+  server.run();
+  
+});
