291568f838bb7e2a845780ff67d78c89ace08b42
Richard Klancer <rpk@pobox.com>
2010-08-27 10:56:02 -0400

    Update proxy.js to be more careful about routes
    This lets proxying work when we vist testrunner urls

--- proxy.js
+++ proxy.js
@@ -15,13 +15,13 @@ httpProxy.createServer(function (req, res, proxy) {
     sys.puts('proxying to rails: ', req.url);
     proxy.proxyRequest(3000, 'localhost', req, res);
   }
-  else if (req.url.match(/^\/geniverse\//) || req.url.match(/^\/chat\//)) {
+  else if (req.url.match(/^\/geniverse\/(:?geniverse)|(:?cache)\//) || req.url.match(/^\/chat\//)) {
     sys.puts('proxying to geniverse.dev.concord.org: ', req.url);
     req.headers.host = 'geniverse.dev.concord.org';
     proxy.proxyRequest(80,'geniverse.dev.concord.org', req, res);
   }
   else {
-    sys.puts('proxying to sc-server: ', req.url);
+    //sys.puts('proxying to sc-server: ', req.url);
     proxy.proxyRequest(4020, 'localhost', req, res);
   }
 }).listen(9000);
