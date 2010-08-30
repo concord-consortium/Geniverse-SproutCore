1db55ed3fbbc2bc6e03bd188c73cdcc64170c148
Richard Klancer <rpk@pobox.com>
2010-08-30 12:42:05 -0400

    Wait for GWT to load before kicking off test

--- apps/geniverse/tests/controllers/gwt.js
+++ apps/geniverse/tests/controllers/gwt.js
@@ -4,14 +4,29 @@
 // ==========================================================================
 
 
-/*globals Geniverse module test ok equals same stop start */
+/*globals Geniverse module test ok equals same stop start checkGWTReadiness runTest */
 
 module("Geniverse.gwt");
 
-test("basic gwt test", function() {
+function checkGWTReadiness() {
+  if (Geniverse.gwtController.get('isReady')) {
+    Geniverse.gwtController.removeObserver('isReady', window, checkGWTReadiness);
+    Geniverse.set('isLoaded', YES);
+    start();
+    runTest();
+  }
+}
+
+test("basic gwt test", function () {
+  stop(5000);
+  Geniverse.gwtController.addObserver('isReady', window, checkGWTReadiness);
+  checkGWTReadiness();
+});
+
+function runTest() {
+  stop(5000);
   Geniverse.gwtController.generateRandomDragon(function (org){
     start();
     ok(true, "We got called back");
   });
-  stop(1000);
-});
+}
