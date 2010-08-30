a68ed06a1a5bfe1351c0558405c53addbfb08308
Richard Klancer <rpk@pobox.com>
2010-08-30 15:44:43 -0400

    Pretty up breedDragonController a little

--- apps/geniverse/controllers/breed_dragon.js
+++ apps/geniverse/controllers/breed_dragon.js
@@ -13,77 +13,66 @@
 Geniverse.breedDragonController = SC.Controller.create(
 /** @scope Geniverse.breedDragonController.prototype */ {
 
-  latestChild: null,
-  
-  mother: null,
-  
-  father: null,
-  
-  child: null,
-  
-  loadTimer: null,
-  
   breedButtonTitle: 'Breed',
-  
   initParentsImmediately: YES,
-  
   gwtReadyBinding: 'Geniverse.gwtController.isReady',
   
-  initParents: function() {
+  mother: null,
+  father: null,
+  child: null,
+  
+  initParents: function () {
     var self = this;
     
-    function setMother(dragon){
-        SC.RunLoop.begin();
-        self.set('mother', dragon);
-        SC.RunLoop.end();
+    function setMother(dragon) {
+      SC.RunLoop.begin();
+      self.set('mother', dragon);
+      SC.RunLoop.end();
     }
     
-    function setFather(dragon){
-        SC.RunLoop.begin();
-        self.set('father', dragon);
-        SC.RunLoop.end();
+    function setFather(dragon) {
+      SC.RunLoop.begin();
+      self.set('father', dragon);
+      SC.RunLoop.end();
     }
+    
     if (this.get('gwtReady') == YES && this.get('initParentsImmediately') == YES) {
       SC.Logger.log('gwt ready. initializing parents');
-      var allelesF = Geniverse.activityController.getInitialAlleles('f');
-      if (allelesF !== undefined && allelesF !== null){
-        Geniverse.gwtController.generateDragonWithAlleles(allelesF, 1, 'Mother', setMother);
-      } else {
+      
+      var alleles = Geniverse.activityController.getInitialAlleles('f');
+      if (typeof(alleles) === 'string') {
+        Geniverse.gwtController.generateDragonWithAlleles(alleles, 1, 'Mother', setMother);
+      } 
+      else {
         Geniverse.gwtController.generateDragon(1, 'Mother', setMother);
       }
       
-      var allelesM = Geniverse.activityController.getInitialAlleles('m');
-      if (allelesM !== undefined && allelesM !== null){
-        Geniverse.gwtController.generateDragonWithAlleles(allelesM, 0, 'Father', setFather);
-      } else {
+      alleles = Geniverse.activityController.getInitialAlleles('m');
+      if (typeof(alleles) === 'string') {
+        Geniverse.gwtController.generateDragonWithAlleles(alleles, 0, 'Father', setFather);
+      } 
+      else {
         Geniverse.gwtController.generateDragon(0, 'Father', setFather);
       }
-      
     }
   }.observes('gwtReady'),
+ 
   
-  breed: function() {
+  breed: function () {
     var self = this;
-    var count = 0;
-    // SC.Logger.group("Breed 20 dragons");
-    // SC.Logger.log("Breeding 20 dragons...");
-    this.set('breedButtonTitle', 'Generating...');
-    Geniverse.eggsController.removeAllEggs(); //clear the breeding pen
-    var handleChild = function(child) {
-      count += 1;
+    var nEggs = 0;
+    this.set('breedButtonTitle', 'Breeding...');
+    Geniverse.eggsController.removeAllEggs(); // clear the breeding pen
+    
+    function didCreateChild(child) {
       SC.RunLoop.begin();
-      // SC.Logger.info("Bred " + count + " child: " + child.get('sex'));
       child.set('isEgg', true);
       self.set('child', child);
-
-      if (count == 20) {
-        if (self.get('breedButtonTitle') !== 'Breed') {
-          self.set('breedButtonTitle', 'Breed');
-        }
-        //SC.Logger.groupEnd();
-      }
+      if (++nEggs >= 20) self.set('breedButtonTitle', 'Breed');
       SC.RunLoop.end();
-    };
-    Geniverse.gwtController.breedOrganisms(20, this.get('mother'), this.get('father'), handleChild);
+    }
+    
+    Geniverse.gwtController.breedOrganisms(20, this.get('mother'), this.get('father'), didCreateChild);
   }
+  
 });
