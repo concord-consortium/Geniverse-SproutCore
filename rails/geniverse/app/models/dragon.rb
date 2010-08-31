6a70ddbc777b1a35b62391734d1c3d92c73a7982
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-08-31 08:53:30 -0700

    Added annotate, ci_reporter, and selenium-webdriver gems. Annotated Rails.

--- rails/geniverse/app/models/dragon.rb
+++ rails/geniverse/app/models/dragon.rb
@@ -6,3 +6,21 @@ class Dragon < ActiveRecord::Base
   
   belongs_to :user
 end
+
+# == Schema Information
+#
+# Table name: dragons
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  sex        :integer
+#  alleles    :string(255)
+#  imageURL   :string(255)
+#  mother_id  :integer
+#  father_id  :integer
+#  bred       :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
