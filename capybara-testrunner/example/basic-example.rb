3245cf7aa3e9d67b3272859e551f1c5ef37bcd7d
Scott Cytacki <scott@concord.org>
2010-09-08 08:59:21 -0400

    converted local capybara-testrunner to a submodule
    The local one had a change to handle .js files in the top level of tests folders.  That is now incorportated.  It also had a hard coding of the host to be sc.local.  That can be supported by passing the -s sc.local option to run-tests.rb

--- capybara-testrunner/example/basic-example.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara'
-require 'capybara/dsl'
-
-Capybara.current_driver = :selenium
-Capybara.app_host = "http://localhost:4020"
-include Capybara
-visit("/cc/en/current/tests/views.html")
-results = evaluate_script('CoreTest.plan.results')
-puts results.inspect
--- capybara-testrunner/test/test-transform.rb
+++
@@ -1,7 +0,0 @@
-$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-
-require 'yaml'
-require 'transform-results'
-
-results = YAML.load_file( 'example-hash.yml' )
-TransformResults.transform(results, $stdout)
