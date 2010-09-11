81e06eb20dd95454062b7d9ce43e6f68e3bfd941
Scott Cytacki <scott@concord.org>
2010-09-10 22:53:12 -0400

    auto free tcp port selection
    By making this automatically find and use a free port it makes it easier to use locally and on hudson.

--- proxy/sc-unit-tests.rb
+++ proxy/sc-unit-tests.rb
@@ -19,24 +19,57 @@ require 'daemon_controller'
 require 'socket'
 require 'fileutils'
 
-port = ENV['SC_SERVER_PORT'] ? ENV['SC_SERVER_PORT'].to_i : 4020
+def find_free_port(initial_port)
+  port = initial_port
+
+  until free_port?(port)
+    port += 1
+  end
+
+  port
+end
+
+def free_port?(port)
+  s = TCPServer.new('127.0.0.1', port)
+  s.close
+  # sproutcore binds to 0.0.0.0 by default so make sure that port isn't taken either
+  s = TCPServer.new('0.0.0.0', port)
+  s.close
+  true
+rescue SocketError, Errno::EADDRINUSE
+  false
+end
+
+def get_port(env_variable, default_port)
+  if ENV[env_variable]
+    sc_server_port = ENV[env_variable].to_i
+  else
+    sc_server_port = find_free_port(default_port);
+  end
+end
+
+# remove old test results rm reports/*.xml
+system("git clean -f #{@options[:results_dir]}")
+
+sc_server_port = get_port('SC_SERVER_PORT', 4020)
+puts "sc-server port: #{sc_server_port}"
+
 sc_server_cmd = ['sc-server',
        '--daemonize',   # run in the background
        "--pid='#{File.join(Dir.pwd, 'server.pid')}'", # save the pid to the server.pid file
        "--logfile='#{File.join(Dir.pwd, 'server.log')}'", # save the log messages to  server.log
-       "--port=#{port}",
-       "--host=0.0.0.0"]  # this is necessary on our ci server because otherwise it only binds to ipv6 address
+       "--port=#{sc_server_port}",
+       "--host=127.0.0.1"]  # this used to be 0.0.0.0 but that seems unnecessarily insecure, that 0.0.0.0
+                            # was necessary on our ci server because otherwise it only binds to ipv6 address
+                            # hopeully 127.0.0.1 will also work
 sc_server = DaemonController.new(
    :identifier    => 'SproutCore Server',
    :start_command => sc_server_cmd.join(' '),
-   :ping_command  => lambda { TCPSocket.new('localhost', port) },
+   :ping_command  => lambda { TCPSocket.new('127.0.0.1', sc_server_port) },
    :pid_file      => 'server.pid',
    :log_file      => 'server.log',
    :start_timeout => 25
 )
-
-# remove old test results rm reports/*.xml
-system("git clean -f #{@options[:results_dir]}")
 
 sc_server.start
 
@@ -44,14 +77,15 @@ $:.unshift File.dirname(__FILE__)
 
 require 'apache_config'
 
-apache_port = ENV['APACHE_PROXY_PORT'] ? ENV['APACHE_PROXY_PORT'].to_i : 1234
+apache_port = get_port('APACHE_PROXY_PORT', 1234);
 
 apache = ApacheConfig.new {
   x_instance_home File.expand_path(File.dirname(__FILE__))
-  listen apache_port
+  x_port apache_port
+  x_host '127.0.0.1'
   x_proxy "/geniverse/geniverse http://geniverse.dev.concord.org/geniverse/geniverse"
   x_proxy "/chat/      http://geniverse.dev.concord.org/chat/"
-  x_proxy "/          http://127.0.0.1:#{ENV['SC_SERVER_PORT'] || 4020}/"
+  x_proxy "/          http://127.0.0.1:#{sc_server_port}/"
 }
 
 apache.controller.start
@@ -64,7 +98,7 @@ run_tests_cmd = ["ruby -rubygems 'capybara-testrunner/run-tests.rb'",
                  "-o #{@options[:results_dir]}"]
 if @options[:interactive] 
   require 'highline/import'
-  puts "Server running at: http://localhost:#{apache_port}"
+  puts "Server running at: http://127.0.0.1:#{apache_port}"
   ask('Press enter to continue');
 else
   system(run_tests_cmd.join(' '))
