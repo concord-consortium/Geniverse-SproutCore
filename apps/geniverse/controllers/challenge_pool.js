2d5ec5974f3d264cd30ccd5a290f3386ae752e53
Aaron Unger <aunger@concord.org>
2010-09-29 10:28:42 -0400

    Add controller for a "challenge pool" (ie starting, read-only set of dragons)

---
+++ apps/geniverse/controllers/challenge_pool.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Geniverse.challengePoolController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Geniverse */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Geniverse.challengePoolController = SC.ArrayController.create(
+  SC.CollectionViewDelegate,
+/** @scope Geniverse.challengePoolController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
