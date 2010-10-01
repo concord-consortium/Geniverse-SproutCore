5d66881fc5a80e603ca22f371ba7d57c3d7e8120
Noah Paessel <knowuh@gmail.com>
2010-10-01 15:30:08 -0400

    statics mixed into breedview.

--- apps/geniverse/views/breed_dragon.js
+++ apps/geniverse/views/breed_dragon.js
@@ -23,7 +23,7 @@ Geniverse.BreedDragonView = SC.View.extend(
   
   childBinding: 'Geniverse.breedDragonController.child',
 
-	childViews: 'fatherView motherView childView fatherLabel motherLabel childLabel breedButtonView'.w(),
+	childViews: 'fatherView motherView childView fatherLabel motherLabel childLabel breedButtonView statsLabel'.w(),
 	
 	hasParentsBinding: 'Geniverse.breedDragonController.hasParents',
 	
@@ -92,6 +92,33 @@ Geniverse.BreedDragonView = SC.View.extend(
 		title: function () {
 		  return this.get('isBreeding') ? 'Breeding...' :  'Breed';
 		}.property('isBreeding').cacheable()
+	}),
+	
+  statsLabel: SC.View.design({
+		layout: {centerX: 0, top: 30, width: 140, height: 68},
+		classNames: "statsLabel".w(),
+    childViews: "red green yellow purple".w(),
+
+    red: SC.LabelView.design({
+      layout: {centerX: 0, top: 0, width: 140, height: 18},
+      classNames: "red".w(),
+	    valueBinding: 'Geniverse.breedDragonController.parentStats.colors.red'
+    }),
+    green: SC.LabelView.design({
+      layout: {centerX: 0, top: 18, width: 140, height: 18},
+      classNames: "green".w(),
+	    valueBinding: 'Geniverse.breedDragonController.parentStats.colors.green'
+    }),
+    yellow: SC.LabelView.design({
+      layout: {centerX: 0, top: 36, width: 140, height: 18},
+      classNames: "yellow".w(),
+	    valueBinding: 'Geniverse.breedDragonController.parentStats.colors.yellow'
+    }),
+    purple: SC.LabelView.design({
+      layout: {centerX: 0, top: 54, width: 140, height: 18},
+      classNames: "purple".w(),
+	    valueBinding: 'Geniverse.breedDragonController.parentStats.colors.purple'
+    }),
 	}),
 	
 	viewDidResize: function() {
