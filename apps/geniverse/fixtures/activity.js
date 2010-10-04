6d6a3b6ef2943fcea518a46f1e59a2c9cc086d01
Aaron Unger <aunger@concord.org>
2010-10-04 09:23:59 -0400

    4 starting dragons, 4 people per group

--- apps/geniverse/fixtures/activity.js
+++ apps/geniverse/fixtures/activity.js
@@ -11,25 +11,17 @@ Geniverse.Activity.FIXTURES = [
   { guid: 1,
     title: 'Geniverse Activity',
     baseChannelName: 'geniverse-chat-trial-b',
-    maxUsersInRoom: 2,
+    maxUsersInRoom: 4,
     initialAlleles: "[" + 
-      "[{alleles: 'a:H,b:h,a:W,b:w,a:L,b:L,a:T,b:t,a:f,b:f,a:B', sex: 1, name: 'Starting Mother'}," +
-       "{alleles: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:t,a:f,b:f,a:B', sex: 0, name: 'Starting Father'}," +
-       "{alleles: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:t,a:f,b:f,a:b', sex: 0, name: 'Starting Father2'}]," +
-      "[{alleles: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f,a:B', sex: 1, name: 'Starting Mother'}, {alleles: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f,a:B', sex: 0, name: 'Starting Father'}] ]",
-    // initialAlleles: [
-    //                               {m: 'a:H,b:h,a:W,b:w,a:L,b:L,a:T,b:t,a:f,b:f', f: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:t,a:f,b:f'}
-    //                             ],
-    // initialAlleles: [
-    //                       {m: 'a:H,b:h,a:w,b:w,a:L,b:L,a:t,b:t,a:F,b:F', f: 'a:H,b:h,a:w,b:w,a:L,b:L,a:t,b:t,a:F,b:F'},
-    //                       {m: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f', f: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f'},
-    //                       {m: 'a:H,b:H,a:w,b:w,a:L,b:l,a:t,b:t,a:F,b:F', f: 'a:H,b:H,a:w,b:w,a:L,b:l,a:t,b:t,a:F,b:F'},
-    //                       {m: 'a:h,b:h,a:W,b:W,a:l,b:l,a:T,b:t,a:f,b:f', f: 'a:h,b:h,a:W,b:W,a:l,b:l,a:T,b:t,a:f,b:f'},
-    //                       {m: 'a:H,b:H,a:w,b:w,a:L,b:L,a:T,b:T,a:F,b:f', f: 'a:H,b:H,a:w,b:w,a:L,b:L,a:T,b:T,a:F,b:f'},
-    //                       {m: 'a:H,b:h,a:w,b:w,a:L,b:L,a:t,b:t,a:F,b:F', f: 'a:H,b:h,a:w,b:w,a:L,b:L,a:t,b:t,a:F,b:F'},
-    //                       {m: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f', f: 'a:h,b:h,a:W,b:w,a:l,b:l,a:T,b:T,a:f,b:f'},
-    //                       {m: 'a:H,b:H,a:w,b:w,a:L,b:l,a:t,b:t,a:F,b:F', f: 'a:H,b:H,a:w,b:w,a:L,b:l,a:t,b:t,a:F,b:F'}
-                    // ],
+      "[{alleles: 'a:H,b:H,a:B', sex: 1, name: 'Starting Mother'}," +
+       "{alleles: 'a:w,b:w,a:B', sex: 0, name: 'Starting Father'}," +
+       "{alleles: 'a:T,b:T,a:B', sex: 1, name: 'Starting Mother2'}," +
+       "{alleles: 'a:l,b:l,a:B', sex: 0, name: 'Starting Father2'}]," +
+      "[{alleles: 'a:h,b:h,a:B', sex: 1, name: 'Starting Mother'}," +
+       "{alleles: 'a:W,b:W,a:B', sex: 0, name: 'Starting Father'}," +
+       "{alleles: 'a:t,b:t,a:B', sex: 1, name: 'Starting Mother2'}," +
+       "{alleles: 'a:L,b:L,a:B', sex: 0, name: 'Starting Father2'}]" +
+     "]",
     sendBredDragons: NO
   }
 
