bf323944439bb845bd1de1485cd7053c6188193b
Aaron Unger <aunger@concord.org>
2010-10-04 10:30:11 -0400

    Add some lines for permissions management.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -7,4 +7,7 @@ sc-build
 # scp -r tmp/build/static/* otto.concord.org:/web/geniverse.dev.concord.org/static/
 rsync -rlzP tmp/build/static/* otto.concord.org:/web/geniverse.dev.concord.org/static/
 
+ssh otto.concord.org "sudo chown -R apache.users /web/geniverse.dev.concord.org/static"
+ssh otto.concord.org "sudo chmod -R ug+rw /web/geniverse.dev.concord.org/static"
+
 echo "Geniverse build hash: $(sc-build-number geniverse)"
