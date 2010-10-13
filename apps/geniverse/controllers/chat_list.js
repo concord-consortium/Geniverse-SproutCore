0cf0c5f73780369e9c9e5cdc97db092bb541e4f9
Noah Paessel <knowuh@gmail.com>
2010-10-13 15:54:32 -0400

    Our own chatlist controller instead of frameworks.

---
+++ apps/geniverse/controllers/chat_list.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Geniverse.chatListController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/* Geniverse globals CcChat */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Geniverse.chatListController = SC.ArrayController.create(
+/** @scope Geniverse.chatListController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
