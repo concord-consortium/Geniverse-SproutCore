c2232fe927e095f97e34af86c6e48f9124d8e60e
Sam Fentress <sfentress@dhcp135.concord.org>
2010-11-10 17:15:12 -0500

    Add sample main page with button link to training page

--- apps/lab/resources/main_page.js
+++ apps/lab/resources/main_page.js
@@ -11,15 +11,25 @@ Lab.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    title: "Welcome to SproutCore!",
-    childViews: 'labelView'.w(),
+    title: "Welcome to the Geniverse Labs",
+    childViews: 'labelView buttonView'.w(),
     
     labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
+      layout: { top: 50, left: 50, width: 200, height: 18 },
       textAlign: SC.ALIGN_CENTER,
       tagName: "h1", 
       valueBinding: 'Lab.mainPage.mainPane.title'
     }),
+    
+    buttonView: SC.ButtonView.design({
+      layout: { top: 120, left: 80, width: 200 },
+      title: 'Go to the training page',
+      action: 'go'
+    }),
+
+    go: function() {
+      SC.routes.set('location', 'chromosomeTrainingPage');
+    },
     
     focusMainFrom: function(pane) {
       SC.$('title').text(this.get('title'));
