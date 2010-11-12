16075644195b76b779e377f571a9e97f26b89caf
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-11-12 14:09:28 -0800

    Converting static labs main HTML page to main_page.js

--- apps/lab/resources/main_page.js
+++ apps/lab/resources/main_page.js
@@ -1,31 +1,39 @@
 // ==========================================================================
 // Project:   Lab - mainPage
-// Copyright: Â©2010 My Company, Inc.
+// Copyright: ©2010 My Company, Inc.
 // ==========================================================================
 /*globals Lab */
 
-// This page describes the main user interface for your application.  
+// This page describes the main user interface for your application.
 Lab.mainPage = SC.Page.design({
-  
+
   pagePath: 'Lab.mainPage',
   title: 'Lab Main Page',
 
   // The main pane is made visible on screen as soon as your app is loaded.
-  // Add childViews to this pane for views to display immediately on page 
+  // Add childViews to this pane for views to display immediately on page
   // load.
   mainPane: SC.MainPane.design({
     title: "Welcome to the Geniverse Labs",
-    childViews: 'labelView buttonView'.w(),
-    
+    childViews: 'backgroundView mainscreenView labelView buttonView'.w(),
+
+    backgroundView: SC.ImageView.design({
+      value: static_url('bg2.png')
+    }),
+
+    mainscreenView: SC.ImageView.design({
+      value: static_url('mainscreen-bg.png')
+    }),
+
     labelView: SC.LabelView.design({
-      layout: { top: 50, left: 50, width: 200, height: 18 },
+      layout: { top: 6, left: 4, width: 200, height: 18 },
       textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
+      tagName: "h1",
       valueBinding: 'Lab.mainPage.mainPane.title'
     }),
-    
+
     buttonView: SC.ButtonView.design({
-      layout: { top: 120, left: 80, width: 200 },
+      layout: { top: 6, left: 200, width: 200 },
       title: 'Go to the training page',
       action: 'go'
     }),
@@ -33,7 +41,7 @@ Lab.mainPage = SC.Page.design({
     go: function() {
       SC.routes.set('location', 'chromosomeTrainingPage');
     },
-    
+
     focusMainFrom: function(pane) {
       SC.$('title').text(this.get('title'));
     }
