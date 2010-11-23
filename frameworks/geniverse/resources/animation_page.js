7bf892d9689564e3108299bcbc34d8b06b9a168d
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-11-23 09:25:42 -0800

    Added Geniverse.AnimationView and lab#geniverse/animationPage .

---
+++ frameworks/geniverse/resources/animation_page.js
@@ -0,0 +1,73 @@
+// ==========================================================================
+// Project:   Geniverse - animationPage
+// Copyright: ©2010 Concord Consortium
+// ==========================================================================
+/*globals Geniverse, CC, CcChat, java */
+Geniverse.marginSize = 15;
+
+sc_require('views/article');
+sc_require('views/breed_dragon');
+sc_require('views/dragon_bin');
+sc_require('views/dragon_chat_compose');
+sc_require('views/organism');
+sc_require('views/published_articles');
+sc_require('views/login');
+sc_require('views/chromosome_tool');
+sc_require('views/stats');
+sc_require('views/animation_view');
+
+Geniverse.animationPage = SC.Page.design({
+  
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    // defaultResponder: Geniverse,
+    classNames: ['brown'],
+    childViews: 'backgroundView topBar meiosisView'.w(),
+      backgroundView: SC.ImageView.design({
+        value: static_url('lab_background.png'),
+        classNames: ['transparent','scalingimage']
+      }),
+
+    topBar: SC.ToolbarView.design({
+      layout: { top: 0, left: 0, right: 0, height: 36 },
+      classNames: ['brown'],
+      childViews: 'geniverseLabelView welcomeLabelView logoutButton'.w(),
+      anchorLocation: SC.ANCHOR_TOP,
+      
+      geniverseLabelView: SC.LabelView.design({
+        layout: { centerY: 0, height: 24, left: 8, width: 200 },
+        controlSize: SC.LARGE_CONTROL_SIZE,
+        fontWeight: SC.BOLD_WEIGHT,
+        //valueBinding:   'Geniverse.activityController.title'
+        value: "Geniverse Animation"
+      }),
+      
+      welcomeLabelView: SC.LabelView.design({
+        layout: { centerY: 0, height: 24, right: 130, width: 500},
+        fontWeight: SC.BOLD_WEIGHT,
+        textAlign: SC.ALIGN_RIGHT,
+        valueBinding: 'Geniverse.loginController.welcomeMessage',
+        isVisibleBinding: 'Geniverse.appController.userLoggedIn'
+      }),
+
+      logoutButton: SC.ButtonView.design({
+        layout: { centerY: 0, height: 24, right: 12, width: 100 },
+        layerId: 'logOutButton',
+        title:  "Log out",
+        target: 'Geniverse.appController',
+        action: 'logout',
+        isVisibleBinding: 'Geniverse.appController.userLoggedIn'
+      })
+    }),
+    
+    meiosisView: Geniverse.AnimationView.design({
+      layout: { left: 50, top: 50, height: 300, width: 300 },
+//          className: 'meiosis',
+//          classNames: 'meiosis',
+      jsondataurl: static_url('chromosomes.json')
+    })
+  })
+  
+});
