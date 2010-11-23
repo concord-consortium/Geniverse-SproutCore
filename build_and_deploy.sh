949eacefa279a455c68cc69c775d6eb75cb44ef0
Sam Fentress <sfentress@dhcp135.concord.org>
2010-11-23 12:46:54 -0500

    Update build-and-deploy script to use Lab

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -10,8 +10,8 @@ rsync -rlzP tmp/build/static/* otto.concord.org:/web/geniverse.dev.concord.org/s
 ssh otto.concord.org "sudo chown -R apache.users /web/geniverse.dev.concord.org/static"
 ssh otto.concord.org "sudo chmod -R ug+rw /web/geniverse.dev.concord.org/static"
 
-echo "Geniverse build hash: $(sc-build-number geniverse)"
+echo "Lab build hash: $(sc-build-number lab)"
 
 read -p "What label should this be deployed with? " -e -r LABEL
 
-ssh otto.concord.org "rm /web/geniverse.dev.concord.org/sproutcore/${LABEL}; ln -s /web/geniverse.dev.concord.org/static/geniverse/en/$(sc-build-number geniverse) /web/geniverse.dev.concord.org/sproutcore/${LABEL}"
+ssh otto.concord.org "rm /web/geniverse.dev.concord.org/sproutcore/${LABEL}; ln -s /web/geniverse.dev.concord.org/static/lab/en/$(sc-build-number lab) /web/geniverse.dev.concord.org/sproutcore/${LABEL}"
