67a8d5d11aee4d669a3ed4e4c44409ae2a471424
Noah Paessel <knowuh@gmail.com>
2010-11-23 13:03:32 -0500

    Initial checkin of ugly sign-up page. FIXME.

---
+++ apps/lab/resources/sign_up.js
@@ -0,0 +1,35 @@
+// ==========================================================================
+// Project:   Lab - mainPage
+// Copyright: ©2010 Concord Consortium
+// ==========================================================================
+/*globals Lab Geniverse */
+
+// This page describes the main user interface for your application.  
+Lab.signUp = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    defaultResponder: Lab.LOGIN,
+    classNames: ['brown'],
+    
+    title: "Welcome to the Geniverse Labs",
+    childViews: 'background loginView'.w(),
+    
+    background: SC.ImageView.design({
+      layout: { top: 0, left: 0, right: 0 },
+      value: static_url('mainscreen-bg.png'),
+      classNames: ['super-transparent']
+    }),
+
+    
+    loginView: SC.LabelView.design({
+      layout: {centerX: 0, top: Lab.marginSize, width: 200, height: 300},
+      escapeHTML: NO,
+      value: 'first <a href="http://localhost:3001">login</a> to participate '
+    })
+  })
+
+});
+
