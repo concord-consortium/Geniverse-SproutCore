1cb455742b552746ca46f91a234279935e343387
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-11-30 08:30:48 -0800

    Added NoteRecord model.

--- frameworks/geniverse/controllers/notepad.js
+++ frameworks/geniverse/controllers/notepad.js
@@ -15,13 +15,32 @@ sc_require('views/notepad');
 
 Geniverse.notepadController = SC.ObjectController.create(
 /** @scope Geniverse.notepadController.prototype */ {
-  content: "",
   pane: Geniverse.NotepadView,
+  store: Geniverse.store,
   
   showPane: function() {
-    if (!this.get('pane').get('isVisibleInWindow')){
-      this.get('pane').append();
+    var username = Geniverse.loginController.username;
+    if(username){
+      SC.Logger.log("showPane called with username:",username);
+      var query = SC.Query.local(Geniverse.NoteRecord, {conditions: 'username = "' + username + '"'});
+      SC.Logger.log("findNoteRecord: about to call find with query:",query);
+      var noteRecords = this.store.find(query);
+      SC.Logger.log("noteRecords:",noteRecords);
+      SC.Logger.log("noteRecords.get('status'):",noteRecords.get('status'));
+      var noteRecord = noteRecords.firstObject();
+      SC.Logger.log("noteRecord:",noteRecord);
+      if(noteRecord == undefined){
+        SC.Logger.warn("noteRecord was undefined so setting Note Pad content to empty string");
+        this.set('content', "");
+      }else{
+        this.set('content', noteRecord.text);
+      }
+      SC.Logger.log("this.get('content'):",this.get('content'));
+      if (!this.get('pane').get('isVisibleInWindow')){
+        this.get('pane').append();
+      }
+    }else {
+      alert("You must be logged in first to use the Note Pad.");
     }
   }
-
 }) ;
