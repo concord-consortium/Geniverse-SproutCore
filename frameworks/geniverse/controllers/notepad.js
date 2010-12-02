c4c71ee0a700d021f6350439fe89afd9b09ddc91
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-12-02 10:13:02 -0800

    Attempt to start saving changes to user.note. TODO: fix bug where the second commit returns __MIXED__ and throws: uncaught exception: SC.Error:sc300:Busy (-1)

--- frameworks/geniverse/controllers/notepad.js
+++ frameworks/geniverse/controllers/notepad.js
@@ -16,7 +16,10 @@ sc_require('views/notepad');
 Geniverse.notepadController = SC.ObjectController.create(
 /** @scope Geniverse.notepadController.prototype */ {
   pane: Geniverse.NotepadView,
-  contentBinding: 'Geniverse.userController.content.note',
+  contentBinding: 'Geniverse.userController*content.note',
+  wasCommitted: NO,
+  lastCommitTime: 0,
+  commitDelayPeriod: 10000,
   
   showPane: function() {
     var username = Geniverse.loginController.username;
@@ -28,7 +31,34 @@ Geniverse.notepadController = SC.ObjectController.create(
         this.get('pane').append();
       }
     }else {
-      alert("You must be logged in first to use the Note Pad.");
+      alert("You must be logged in first to use your Note Pad.");
     }
-  }
+  },
+
+  contentDidChange: function() {
+//    SC.Logger.log("Geniverse.notepadController.contentDidChange() called");
+//    SC.Logger.log("this.content:", this.content);
+//    SC.Logger.log("Geniverse.userController.content:", Geniverse.userController.content);
+    if(this.wasCommitted != SC.MIXED_STATE){
+      var now = new Date().getTime();
+      // set this.lastCommitTime if this is the first time it is being used
+      if(this.lastCommitTime == 0) this.lastCommitTime = now;
+      var elapsedTime = now - this.lastCommitTime;
+      SC.Logger.log("elapsedTime:",elapsedTime);
+      if(elapsedTime >= this.commitDelayPeriod){
+        // TODO: fix bug where the second commit returns __MIXED__ and throws:
+        // uncaught exception: SC.Error:sc300:Busy (-1)
+        this.wasCommitted = Geniverse.store.commitRecord(
+          Geniverse.User,
+          Geniverse.userController.get('content').get('id'),
+          Geniverse.userController.get('content').get('storeKey')
+        );
+        SC.Logger.log("after commitRecord: this.wasCommitted:", this.wasCommitted);
+      }
+    }
+    if(this.wasCommitted){
+      this.lastCommitTime = new Date().getTime();
+    }
+  }.observes('content')
+
 }) ;
