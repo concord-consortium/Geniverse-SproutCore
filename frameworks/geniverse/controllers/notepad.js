b77885dfe60a686c9ed8e32936780a0f627c46a8
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-12-06 08:15:41 -0800

    Fixed Firefox bug: \n line breaks weren't persisted, are now replaced by \r

--- frameworks/geniverse/controllers/notepad.js
+++ frameworks/geniverse/controllers/notepad.js
@@ -27,10 +27,10 @@ Geniverse.notepadController = SC.ObjectController.create(
       //SC.Logger.log("user's note:",note);
       // Create a new NotepadView instance each time the pane is shown to avoid Firefox TextArea focus bug
       var notepadView = Geniverse.NotepadView.create();
-      ////console.log("created notepadView:",notepadView);
+      //console.log("created notepadView:",notepadView);
       this.set('pane',notepadView);
       var _pane = this.get('pane');
-      ////console.log("this.get('pane'):",_pane);
+      //console.log("this.get('pane'):",_pane);
       if (!_pane.get('isVisibleInWindow')){
         _pane.append();
         this.updateView(this.get('content'));
@@ -42,25 +42,41 @@ Geniverse.notepadController = SC.ObjectController.create(
 
   contentDidChange: function () {
     var _content = this.get('content');
-    ////console.log("contentDidChange to:",_content);
+    //console.log("contentDidChange to:",_content);
     this.updateView(_content);
   }.observes('content'),
 
-  // TODO: fix Firefox bug: Persist linefeeds properly
+  /**
+   * fixes Firefox bug: \n line breaks don't make it to the backend,
+   * so replace them with \r which do get persisted properly
+   * @return a String with \n replaced with \r, if input was null or undefined it is simply returned back
+   * TODO: Move this method into a utility class
+   */
+  fixLineBreaks:function (text) {
+    if(text){
+      var textString = text.toString();
+      if(textString){
+        return textString.replace(/\n/g, "\r");
+      }
+    }
+    return text;
+  },
+
   commitAndRemoveView: function (){
     var _content = this.get('content');
     var _userNote = Geniverse.userController.get('note');
     if(_userNote != _content){
-      ////console.warn("Geniverse.userController.get('note'):",_userNote);
-      ////console.warn("was NOT equal to this.get('content'):",_content);
-      ////console.warn("calling Geniverse.userController.set('note', this.get('content'));");
+      //console.warn("Geniverse.userController.get('note'):",_userNote);
+      //console.warn("was NOT equal to this.get('content'):",_content);
+      //console.warn("calling Geniverse.userController.set('note', this.get('content'));");
       Geniverse.userController.set('note', _content);
     }
-    ////console.log("Geniverse.userController.note:", Geniverse.userController.get('note'));
+    Geniverse.userController.set('note', this.fixLineBreaks(Geniverse.userController.get('note')));
+    //console.log("Geniverse.userController.note:", Geniverse.userController.get('note'));
     var wasCommitted = Geniverse.store.commitRecords();
-    ////console.log("Geniverse.store.commitRecords() returned this.wasCommitted:", this.wasCommitted);
+    //console.log("Geniverse.store.commitRecords() returned this.wasCommitted:", this.wasCommitted);
     var receiver = this.pane.remove();
-    ////console.log("this.pane.remove() returned receiver:", receiver);
+    //console.log("this.pane.remove() returned receiver:", receiver);
   },
 
   updateView: function (newValue) {
