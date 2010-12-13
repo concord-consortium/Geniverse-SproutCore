204a5dfbcb32c41e281815482c8065795b024966
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-12-13 08:34:37 -0800

    Used simple HTML formatting

--- apps/lab/controllers/login.js
+++ apps/lab/controllers/login.js
@@ -175,7 +175,7 @@ Lab.loginController = SC.ObjectController.create(
     var userName = user.get('firstName');
     this.hidePanel();
     this.set('welcomeMessage',"Welcome %@, you are member #%@ in group %@".fmt(userName, member, group));
-    Lab.infoController.display(this.get('welcomeMessage'));
+    Lab.infoController.display("<div><h2>"+this.get('welcomeMessage')+"</h2></div>");
     CcChat.chatController.set('username', user.get('username'));
     Lab.userDefaults.writeDefault('username', user.get('username'));
     this.updateGroupInfo();
