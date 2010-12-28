51334f1a633d41af1e9569e64e1b01fe3f51b3f1
Aaron Unger <aunger@concord.org>
2010-12-28 11:36:37 -0500

    Use -t on ssh so sudo passwords aren't displayed in cleartext

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -7,11 +7,11 @@ sc-build
 # scp -r tmp/build/static/* otto.concord.org:/web/geniverse.dev.concord.org/static/
 rsync -rlzP tmp/build/static/* otto.concord.org:/web/geniverse.dev.concord.org/static/
 
-ssh otto.concord.org "sudo chown -R apache.users /web/geniverse.dev.concord.org/static"
-ssh otto.concord.org "sudo chmod -R ug+rw /web/geniverse.dev.concord.org/static"
+ssh -t otto.concord.org "sudo chown -R apache.users /web/geniverse.dev.concord.org/static"
+ssh -t otto.concord.org "sudo chmod -R ug+rw /web/geniverse.dev.concord.org/static"
 
 echo "Lab build hash: $(sc-build-number lab)"
 
 read -p "What label should this be deployed with? " -e -r LABEL
 
-ssh otto.concord.org "rm /web/geniverse.dev.concord.org/sproutcore/${LABEL}; ln -s /web/geniverse.dev.concord.org/static/lab/en/$(sc-build-number lab) /web/geniverse.dev.concord.org/sproutcore/${LABEL}"
+ssh -t otto.concord.org "rm /web/geniverse.dev.concord.org/sproutcore/${LABEL}; ln -s /web/geniverse.dev.concord.org/static/lab/en/$(sc-build-number lab) /web/geniverse.dev.concord.org/sproutcore/${LABEL}"
