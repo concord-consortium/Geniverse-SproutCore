ce81342fb7bc508973c38545ed8c51bda0feb45d
Aaron Unger <aunger@concord.org>
2011-01-31 16:20:31 -0500

    MatchView now uses a GridView and a custom OrganismView

--- frameworks/geniverse/views/match.js
+++ frameworks/geniverse/views/match.js
@@ -10,120 +10,64 @@
 
   @extends SC.View
 */
+
+sc_require('mixins/match_organism');
+sc_require('mixins/shifted_organism');
+
 Geniverse.MatchView = SC.View.extend(
 /** @scope Geniverse.MatchingView.prototype */ {
 
-  matchDragonsBinding: 'Geniverse.matchController.arrangedObjects',
-  
-  childViews: 'title dragonViews'.w(),
-  
-  title: SC.LabelView.design({
-    classNames: 'container_label'.w(),
-    layout: { top:0, height: 20, left: 0 },
-    controlSize: "bity",
-    textAlign: SC.ALIGN_CENTER,
-    fontWeight: SC.BOLD_WEIGHT,
-    value: "Target Drakes"
-  }),
-  
-  isVertical: NO,
+  matchDragonsControllerPath: 'Geniverse.matchController',
+  dragonSize: 75,
+  dragonExampleView: Geniverse.OrganismView.extend(Geniverse.MatchOrganism, Geniverse.ShiftedOrganism),
+
+  titleView: null,
+  dragonsView: null,
   
   isVisible: function() {
-    var titleWidth = (Geniverse.matchController.get('length') * this.get('layout').height);
-    titleWidth  = (titleWidth < 130 || this.get('isVertical')) ? 130 : titleWidth;
-    this.get('title').get('layout').width = titleWidth;
-    this.get('title').set('textAlign', this.get('isVertical') ? SC.ALIGN_LEFT : SC.ALIGN_CENTER);
-    this.get('title').layoutDidChange();
     return Geniverse.matchController.get('length') > 0;
-  }.property('Geniverse.matchController.arrangedObjects.[]'),
+  }.property('Geniverse.matchController.length'),
   
   updateIsVisible: function(){
     this.propertyDidChange('isVisible');
   }.observes('Geniverse.matchController.arrangedObjects.[]'),
-  
-  dragonViews: SC.View.design({
-    layout: { top:0, bottom: 0, left: 0, right: 0 }
-  }),
-  
-  matchViewsInitialized: NO,
+ 
 
-  updateMatchViews: function () {
-    // add dragon views
-    // FIXME: this seems to get called every time the data store refreshes...
-    if (! this.get('matchViewsInitialized')) {
-      var matchDragons = this.get('matchDragons');
-      this.get('dragonViews').removeAllChildren();
-      for (var i = 0, ii = matchDragons.get('length'); i < ii; i++) {
-        this.addDragonView(matchDragons.objectAt(i), i);
-      }
-      if (matchDragons.get('length') > 0) {
-        this.set('matchViewsInitialized', YES);
-      }
-    }
-  }.observes('*matchDragons.[]'),
-  
-  addDragonView: function (dragon, i) {
-    var height = this.get('layout').height;
-    var width = this.get('layout').width;
-    var top, left;
-    if (!this.get('isVertical')){
-      top = 20;
-      left = (i * height);
-      width = height;
-    } else {
-      top = 20 + (i * width);
-      left = 0;
-    }
-    var dragonView = Geniverse.OrganismView.create({
-      layout: {top: top, height: width, left: left, width: width},
-      content: dragon,
-      isMatched: NO,
-      isDropTarget: YES,
-      mouseDown: function(evt) {      // no selection allowed
-        return NO;
-      },
-      // drop methods: NB: none of these will be called if isDropTarget = NO
-      acceptDragOperation: function(drag, op) {
-        var dragon = this._getSourceDragon(drag);
-        
-        var targetURL = this.get('content').get('imageURL');
-        console.log("targetURL = "+targetURL);
-        
-        if (dragon.get('imageURL') === targetURL){
-          SC.RunLoop.begin();
-            // this.get('parentView').get('proposedDragons').addObject(dragon);
-            this.set('isMatched', YES);
-          SC.RunLoop.end();
-          
-        } else {
-          alert("Those dragons don't look exactly the same.");
-        }
+  createChildViews: function() {
+    var childViews = [];
+    this.dragonsView = this.createChildView(
+      CC.AutoScrollView.design({
+        hasHorizontalScroller: NO,
+        layout: { left: 0, top: 20, right: 0, bottom: 0},
+        contentView: SC.GridView.design({
+          classNames: ['dragon-grid'],
+          contentBinding: this.get('matchDragonsControllerPath')+'.arrangedObjects',
+          selectionBinding: this.get('matchDragonsControllerPath')+'.selection',
+          rowHeight: this.get('dragonSize'),
+          columnWidth: this.get('dragonSize'),
+          canEditContent: NO,
+          exampleView: this.get('dragonExampleView'),
+          isSelectable: NO,
+          dragDataTypes: ['dragon']
+        }),
+        autoScrollTriggerBinding: this.get('matchDragonsControllerPath')+'.length'
+      })
+    );
+    childViews.push(this.dragonsView);
 
-        this._setClassNames();
+    this.titleView = this.createChildView(
+      SC.LabelView.design({
+        classNames: 'container_label'.w(),
+        layout: { top:0, height: 20, left: 0, right: 0, minWidth: 130 },
+        controlSize: "bity",
+        textAlign: SC.ALIGN_CENTER,
+        fontWeight: SC.BOLD_WEIGHT,
+        value: "Target Drakes"
+      })
+    );
+    childViews.push(this.titleView);
 
-        return NO ;
-      },
-
-      dragEntered: function(drag, evt) {
-          SC.RunLoop.begin();
-          this.set('isSelected', YES);
-          SC.RunLoop.end();
-          this._setClassNames();
-      },
-      
-      _setClassNames: function(){
-        var imageView = this.get('imageView');
-        if (this.get('isMatched')){
-          imageView.set('classNames', ['matched']);
-        } else if (this.get('isSelected')) {
-          imageView.set('classNames', ['male-selected']);
-        } else {
-          imageView.set('classNames', ['empty']);
-        }
-        imageView.displayDidChange();
-      }
-    });
-    this.get('dragonViews').appendChild(dragonView);
+    this.set('childViews', childViews);
   }
 
 });
