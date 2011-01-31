ff96d419c3f23e516b219216dfafe459e73dbf53
Aaron Unger <aunger@concord.org>
2011-01-31 10:22:58 -0500

    Fix a 'BUSY' bug when destroying eggs.

--- frameworks/geniverse/controllers/eggs.js
+++ frameworks/geniverse/controllers/eggs.js
@@ -13,44 +13,52 @@
 Geniverse.eggsController = SC.ArrayController.create(
   SC.CollectionViewDelegate,
 /** @scope Geniverse.eggsController.prototype */ {
-  
+
   selectionBinding: 'Geniverse.allSelectedDragonsController.selection',
-    
-    removeAllEggs: function () {
-      if (this.get('content') !== null) {
-        // FIXME We should also destroy all the Organism views associated with these eggs.
-        // Without an explicit destroy() call, they stick around, and GridView doesn't do this automatically
-        this.get('content').forEach(function(egg){
-          // tell the datastore to remove the egg from it's data hashes to save memory
-          // SC.Logger.log("Destroying egg", egg.get('id'));
-          egg.destroy();
-        });
-        Geniverse.store.commitRecords();
-      }
-      this.set('content',[]);
-    },
-   
-    saveData: function() {
+
+  removeAllEggs: function () {
+    if (this.get('content') !== null) {
+      this.get('content').forEach(Geniverse.eggsController._destroyEgg);
       Geniverse.store.commitRecords();
-    }.observes('[]'),
+    }
+    this.set('content',[]);
+  },
 
-    collectionViewDeleteContent: function(view, content, indexes) {
-      // destroy the records
-      var records = indexes.map(function(idx) {
-        return this.objectAt(idx);
-      }, this);
-      records.invoke('destroy');
+  _destroyEgg: function(egg) {
+    // tell the datastore to remove the egg from it's data hashes to save memory
+    // SC.Logger.log("Destroying egg", egg.get('id'));
+    if (egg.get('state') & SC.Record.READY) {
+      egg.removeObserver('state', Geniverse.eggsController, '_destroyEgg');
+      egg.destroy();
+    } else {
+      // egg isn't in a ready state -- add an observer to remove the egg when it is ready.
+      // it's ok to add the same observer multiple times, as SC will detect the duplicate and only
+      // end up with 1 registered observer
+      egg.addObserver('state', Geniverse.eggsController, '_destroyEgg');
+    }
+  },
 
-      var selIndex = indexes.get('min')-1;
-      if (selIndex<0) {
-        selIndex = 0;
-      }
-      this.selectObject(this.objectAt(selIndex));
-    },
+  saveData: function() {
+    Geniverse.store.commitRecords();
+  }.observes('[]'),
 
-    // http://www.pivotaltracker.com/story/show/5298197
-    // rest parents and breeding pool.
-    reset: function() {
-      this.removeAllEggs();
-    }.observes('Geniverse.breedDragonController.resetCount')
-}) ;
+  collectionViewDeleteContent: function(view, content, indexes) {
+    // destroy the records
+    var records = indexes.map(function(idx) {
+      return this.objectAt(idx);
+    }, this);
+    records.invoke('destroy');
+
+    var selIndex = indexes.get('min')-1;
+    if (selIndex<0) {
+      selIndex = 0;
+    }
+    this.selectObject(this.objectAt(selIndex));
+  },
+
+  // http://www.pivotaltracker.com/story/show/5298197
+  // rest parents and breeding pool.
+  reset: function() {
+    this.removeAllEggs();
+  }.observes('Geniverse.breedDragonController.resetCount')
+});
