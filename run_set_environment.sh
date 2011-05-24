d0c19cf758a71f2c437018d0045f6e8711e4b66b
Aaron Unger <aunger@concord.org>
2011-05-24 12:12:11 -0400

    Have bundler install into a directory, so we don't have to worry about our outer gem environment.

--- run_set_environment.sh
+++ run_set_environment.sh
@@ -19,8 +19,8 @@ setTestingEnv() {
     export CI_RSPEC_LOADER=${WORKSPACE}/bundled_gems/ci_reporter-1.6.3/lib/ci/reporter/rake/rspec_loader
 
     # set up bundler
-    sh -c 'bundle install'
-    sh -c 'cd rails/geniverse && bundle install'
+    sh -c 'bundle install --path bundled_gems'
+    sh -c 'cd rails/geniverse && bundle install --path ../../bundled_gems'
 
     # make sure the rails db is ready
     sh -c 'cd rails/geniverse && rake db:setup --trace'
