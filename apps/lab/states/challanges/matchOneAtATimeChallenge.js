87c8ef509a7d38195c4863465ecd7d9d3f0b844e
Sam Fentress <sfentress@concord.org>
2011-06-17 14:42:02 -0400

    Add ability to enter specific challenge state based on activity pageType

---
+++ apps/lab/states/challanges/matchOneAtATimeChallenge.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Lab
+// Copyright: ©2010 Concord Consortium
+// ==========================================================================
+/*globals Lab Geniverse CcChat window Ki*/
+
+Lab.matchOneAtATimeChallange = Ki.State.extend({
+  
+  enterState: function() { 
+  },
+  
+  startTrial: function() {
+    
+  },
+  
+  exitState: function() { 
+  }
+  
+});
--- apps/lab/states/inActivity.js
+++ apps/lab/states/inActivity.js
@@ -2,17 +2,34 @@
 // Project:   Lab
 // Copyright: ©2010 Concord Consortium
 // ==========================================================================
-/*globals Lab Geniverse CcChat window Ki*/
+/*globals Lab Geniverse CcChat window Ki gotoState*/
 
 Lab.inActivity = Ki.State.extend({
   
+  substatesAreConcurrent: NO,
+  
+  matchOneAtATimeChallange: Ki.State.plugin('Lab.matchOneAtATimeChallange'),
+  
+  currentChallenge: null,
+  
   enterState: function() { 
+    Geniverse.activityController.addObserver('content', this, this.activityLoaded);
     Lab.makeFirstResponder(Lab.ACTIVITY);
     Lab.ACTIVITY.gotoActivity();
   },
   
+  activityLoaded: function(){
+    Geniverse.activityController.removeObserver('content', this.activityLoaded);
+    
+    var pageType = Geniverse.activityController.get('pageType');
+    var challengeType = Lab[pageType].get('challengeType')
+    if (!!challengeType) {
+      this.gotoState(challengeType);
+    }
+  },
+  
   startTrial: function() {
-    // placeholder
+    gotoState('inTrial');
   },
   
   exitState: function() { 
