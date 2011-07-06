a0a09da8fd81b37752b3a91640ee180912dafad5
Aaron Unger <aunger@concord.org>
2011-07-06 13:16:00 -0400

    Deploy script change to support not using bundler.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -x $SKIP_BUNDLER ]; then
+  CMD_PREFIX="bundle exec"
+else
+  CMD_PREFIX=""
+fi
+
 if [ -x $1 ]; then
   echo "You must specify which server to install to.\nSupported servers: production, dev\n\nUsage: $0 [server]"
   exit 1
@@ -23,13 +29,13 @@ case "$1" in
 esac
 
 rm -rf tmp/
-bundle exec sc-build
+$CMD_PREFIX sc-build
 
 # If you don't have rsync, use scp instead
 # scp -r tmp/build/static/* geniverse@$SERVER:$SERVER_PATH/
 rsync -rlzP tmp/build/static/* geniverse@$SERVER:$SERVER_PATH/
 
-BUILD_NUM=$(bundle exec sc-build-number lab)
+BUILD_NUM=$($CMD_PREFIX sc-build-number lab)
 echo "Lab build hash: $BUILD_NUM"
 
 read -p "What label should this be deployed with? " -e -r LABEL
