7d78fcc46923b1dd0c17a84a4a97332de67d38f1
Sam Fentress <sfentress@concord.org>
2012-01-11 12:25:37 -0500

    Add stars to challenge success message

--- apps/lab/states/challenges/matchOneAtATimeChallenge.js
+++ apps/lab/states/challenges/matchOneAtATimeChallenge.js
@@ -2,7 +2,7 @@
 // Project:   Lab
 // Copyright: ©2010 Concord Consortium
 // ==========================================================================
-/*globals Lab Geniverse CcChat window Ki YES NO SC*/
+/*globals Lab Geniverse CcChat window Ki YES NO SC static_url*/
 
 Lab.matchOneAtATimeChallenge = Ki.State.extend({
   
@@ -11,6 +11,8 @@ Lab.matchOneAtATimeChallenge = Ki.State.extend({
   challengeComplete: NO,
   
   organismView: null,
+  
+  starsEarned: 0,
   
   enterState: function() { 
     // for now, we assume that there are match dragons
@@ -33,6 +35,7 @@ Lab.matchOneAtATimeChallenge = Ki.State.extend({
     var stars = Geniverse.scoringController.get('achievedChallengeStars');
     var pageId = Geniverse.activityController.get('guid');
     Geniverse.userController.setPageStars(pageId, stars);
+    this.starsEarned = stars;
   },
   
   setTargetScore: function() {
@@ -115,20 +118,31 @@ Lab.matchOneAtATimeChallenge = Ki.State.extend({
   },
 
   _challengeComplete: function() {
+    
+    this.endChallenge();
+    
     // Notify the user that they're done
+    var starImageUrl = this.starsEarned === 3 ? static_url('three-star.png') : 
+          this.starsEarned === 2 ? static_url('two-star.png') : static_url('one-star.png');
+    var starsMessage = "<img src='"+starImageUrl+"' class='centered-block'/>\n"+
+                       "You earned "+this.starsEarned+" star" + (this.starsEarned === 1 ? "" : "s") + "!\n\n";
     var moveOnMessage = (!!Geniverse.activityController.getNextActivity()) ? 
       "Move on to the next challenge using the green arrow below." :
       "Go back to the case log using the button at the top left to go to a new case.";
-    SC.AlertPane.extend({layout: {top: 0, centerX: 0, width: 350, height: 100 }}).plain(
+    SC.AlertPane.extend({
+      layout: {top: 0, centerX: 0, width: 350, height: 100 },
+      displayDescription: function() {
+        var desc = this.get('description');
+        if (!desc || desc.length === 0) {return desc;} 
+        return '<p class="description">' + desc.split('\n').join('</p><p class="description">') + '</p>';
+      }.property('description').cacheable()}).plain(
       "Good work!", 
-      "You've completed all the trials in this challenge!\n"+moveOnMessage,
+      "You've completed all the trials in this challenge!\n"+starsMessage+moveOnMessage,
       "",
       "OK",
       "",
       this
     );
-
-    this.endChallenge();
   },
 
   _updateNumTrials: function() {
