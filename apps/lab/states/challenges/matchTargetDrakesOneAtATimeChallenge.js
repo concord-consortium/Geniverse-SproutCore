d35c7c1fbbb96a2f35822453463f0d8b5b0f6aaf
Aaron Unger <aaron.unger@gmail.com>
2012-01-13 15:04:04 -0800

    Convert state to extend the new challenge state.

--- apps/lab/states/challenges/matchTargetDrakesOneAtATimeChallenge.js
+++ apps/lab/states/challenges/matchTargetDrakesOneAtATimeChallenge.js
@@ -4,37 +4,16 @@
 // ==========================================================================
 /*globals Lab Geniverse CcChat window Ki YES NO SC*/
 
-Lab.matchTargetDrakesOneAtATimeChallenge = Ki.State.extend({
+Lab.matchTargetDrakesOneAtATimeChallenge = Lab.challenge.extend({
 
   successfulMatch: NO,
 
-  challengeComplete: NO,
-
-  starsEarned: 0,
-
-  enterState: function() { 
-    // for now, we assume that there are match dragons
-    this.startChallenge();
-  },
-
   startChallenge: function() {
-    this.statechart.getState('inActivity').blockNextNavButton(true);
+    sc_super();
     Lab.ACTIVITY.set('LOAD_CHALLENGE_DRAKES', NO);
-    this.set('challengeComplete', NO);
 
     // This needs to happen after the match dragons are loaded into the controller....
     Geniverse.matchController.addObserver('arrangedObjects.length', this._updateNumTrials);
-  },
-
-  endChallenge: function() {
-    this.challengeComplete = YES;
-    this.statechart.getState('inActivity').blockNextNavButton(false);
-
-    // Award the stars
-    var stars = Geniverse.scoringController.get('achievedChallengeStars');
-    var pageId = Geniverse.activityController.get('guid');
-    Geniverse.userController.setPageStars(pageId, stars);
-    this.starsEarned = stars;
   },
 
   // Annoyingly, actions can only be called with a max of two arguments,
@@ -81,39 +60,8 @@ Lab.matchTargetDrakesOneAtATimeChallenge = Ki.State.extend({
     }
   },
 
-  _challengeComplete: function() {
-    this.endChallenge();
-
-    // Notify the user that they're done
-    var starImageUrl = this.starsEarned === 3 ? static_url('three-star.png') : 
-          this.starsEarned === 2 ? static_url('two-star.png') : static_url('one-star.png');
-    var starsMessage = "<img src='"+starImageUrl+"' class='centered-block'/>\n"+
-                       "You earned "+this.starsEarned+" star" + (this.starsEarned === 1 ? "" : "s") + "!\n\n";
-    var moveOnMessage = (!!Geniverse.activityController.getNextActivity()) ? 
-      "Move on to the next challenge using the green arrow below." :
-      "Go back to the case log using the button at the top left to go to a new case.";
-
-    SC.AlertPane.extend({
-      layout: {top: 0, centerX: 0, width: 350, height: 100 },
-      displayDescription: function() {
-        var desc = this.get('description');
-        if (!desc || desc.length === 0) {return desc;}
-        return '<p class="description">' + desc.split('\n').join('</p><p class="description">') + '</p>';
-      }.property('description').cacheable()}).plain(
-      "Good work!",
-      "You've completed all the trials in this challenge!\n"+starsMessage+moveOnMessage,
-      "",
-      "OK",
-      "",
-      this
-    );
-  },
-
-  _updateNumTrials: function() {
-    Geniverse.scoringController.set('numberOfTrials', Geniverse.matchController.getPath('arrangedObjects.length'));
-  },
-
   exitState: function() {
+    sc_super();
     Geniverse.matchController.removeObserver('arrangedObjects.length', this._updateNumTrials);
   }
 
