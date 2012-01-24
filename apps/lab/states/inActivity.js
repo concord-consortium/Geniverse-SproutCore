6a7bfe444a19850281fbb777d86a5447acca3aaa
Richard Klancer <rpk@pobox.com>
2012-01-24 14:51:49 -0500

    Add 'gotoCaselog' event to statechart; complain about Ki's API.

--- apps/lab/states/inActivity.js
+++ apps/lab/states/inActivity.js
@@ -41,6 +41,8 @@ Lab.inActivity = Ki.State.extend({
       Lab.makeFirstResponder(Lab.ACTIVITY);
       Lab.ACTIVITY.gotoActivity();
     }
+    // Indicate that we handled 'gotoActivity' action so that our parent state (atLocation) doesn't try to handle it.
+    return YES;
   },
   
   activityLoaded: function(){
@@ -144,7 +146,7 @@ Lab.inActivity = Ki.State.extend({
     Lab.statechart.getState('atLocation').startPage = "home";
     this.gotoState('inHomePage');
   },
-  
+
   exitState: function() {
     // Make sure any observers we might have added during in state are removed.
 
