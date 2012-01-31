55b0f86cfe41467b5741551fcae369c5a796adbf
Richard Klancer <rpk@pobox.com>
2012-01-31 11:17:45 -0500

    Caselog page correctly navigates levels

--- apps/lab/controllers/caselog.js
+++ apps/lab/controllers/caselog.js
@@ -10,9 +10,27 @@ Lab.LEVEL_TRAINING   = 0;
 Lab.LEVEL_APPRENTICE = 1;
 Lab.LEVEL_JOURNEYMAN = 2;
 Lab.LEVEL_MASTER     = 3;
+Lab.LEVEL_MEIOSIS    = 4;
+Lab.LEVEL_DNA        = 5;
 
 Lab.caselogController = SC.Object.create({
-  levels: Lab.caselogData
+  
+  levels: Lab.caselogData,
+
+  levelNames: ['training', 'apprentice', 'journeyman', 'master', 'meiosis', 'dna'],
+  
+  currentLevel: Lab.LEVEL_TRAINING,
+  
+  currentLevelName: function(key, value) {
+    if (typeof value === 'undefined') {
+      // get
+      return this.get('levelNames')[this.get('currentLevel')]; 
+    }
+    else {
+      this.set('currentLevel', this.levelNames.lastIndexOf(value));
+    }
+  }.property('currentLevel')
+
 });
 
 // e.g., 
