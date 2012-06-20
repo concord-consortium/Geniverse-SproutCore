cbf8da4ba3fff85e4adf5c798bf32f2e26a8bde8
Sam Fentress <sfentress@concord.org>
2012-06-20 11:14:26 -0400

    Use activity route as key for stars. === BREAKS OLD LEARNER DATA! ===
    Update caselog to use only case json file, not to download activities.
    
    We now key stars off of activity route instead of activity id, since
    we'd have to ask the back-end for the activity id, and we don't want
    to download all activities. Also stars will stay with user if new activity
    uses an old route, and will go away if an author changes an activity 
    record's route, which seems like better behavior.

--- apps/lab/states/challenges/argumentationChallenge.js
+++ apps/lab/states/challenges/argumentationChallenge.js
@@ -5,35 +5,35 @@
 /*globals Lab Geniverse CcChat window Ki YES NO SC*/
 
 Lab.argumentationChallenge = Ki.State.extend({
-  
+
   challengeComplete: NO,
   challengeWasAlreadyComplete: NO,
-  
-  enterState: function() { 
+
+  enterState: function() {
     this.startChallenge();
   },
-  
+
   startChallenge: function() {
     this.set('challengeComplete', NO);
     this.set('challengeWasAlreadyComplete', NO);
-    
+
     this.get('statechart').sendAction('blockNextNavButton');
     Lab.ACTIVITY.set('LOAD_CHALLENGE_DRAKES', YES);
   },
-  
+
   endChallenge: function() {
     this.set('challengeComplete', YES);
     this.get('statechart').sendAction('unblockNextNavButton');
 
     // Award a "star" for completion
-    var pageId = Geniverse.activityController.get('guid');
+    var pageId = Geniverse.activityController.get('route');
     Geniverse.userController.setPageStars(pageId, 1);
     Geniverse.store.commitRecords();
 
     // why can't bindings in SC work as advertised?
     Lab.caselogController.propertyDidChange("userMetadata");
   },
-  
+
   didSendBlogPost: function() {
     if (this.get('challengeComplete')) {
       this.set('challengeWasAlreadyComplete', YES);
@@ -41,16 +41,16 @@ Lab.argumentationChallenge = Ki.State.extend({
       this.endChallenge();
     }
   },
-  
+
   // Needs to be DRY'd up - see match TargetDrakesListChallenge.js
   checkMatchDragon: function(dragons, view) {
     Geniverse.scoringController.incrementScore(1);
-    
+
     if (Geniverse.matchController.doesMatch(dragons[0], dragons[1])) {
       view.setPath('content.hasBeenMatched', YES);
       view._setClassNames();
       SC.AlertPane.extend({layout: {top: 0, centerX: 0, width: 300, height: 100 }}).plain(
-        "Good work!", 
+        "Good work!",
         "The drake you have created matches the target drake.",
         "",
         "OK",
@@ -59,7 +59,7 @@ Lab.argumentationChallenge = Ki.State.extend({
       );
     } else {
       SC.AlertPane.extend({layout: {top: 0, centerX: 0, width: 300, height: 100 }}).error(
-        "That's not the drake!", 
+        "That's not the drake!",
         "The drake you have created doesn't match the target drake. Please try again.",
         "",
         "Try again",
@@ -69,7 +69,7 @@ Lab.argumentationChallenge = Ki.State.extend({
     }
   },
 
-  exitState: function() { 
+  exitState: function() {
   }
-  
+
 });
