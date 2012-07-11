f5d37023e9fdf85385c8628508e99f18efab1849
Sam Fentress <sfentress@concord.org>
2012-07-11 09:09:37 -0400

    Fix error removing iframe from view

--- frameworks/geniverse/controllers/blog_post.js
+++ frameworks/geniverse/controllers/blog_post.js
@@ -14,15 +14,15 @@ sc_require('views/blog_post');
 
 Geniverse.blogPostController = SC.Controller.create(
 /** @scope Geniverse.chromosomeToolController.prototype */ {
-  
+
   title: "",
-  
+
   content1: "",
-  
+
   content2: "",
-  
+
   content3: "",
-  
+
   content: function() {
     var content =
       "<b>Evidence</b>" +
@@ -34,19 +34,19 @@ Geniverse.blogPostController = SC.Controller.create(
       "<p>" +
       "${reasoning}" +
       "</p>";
-      
+
     content = content
                 .replace(/\$\{evidence\}/g, this.get('content1'))
                 .replace(/\$\{url\}/g, this.get('content2'))
                 .replace(/\$\{reasoning\}/g, this.get('content3'));
-                
+
     return content;
   }.property('content1', 'content2', 'content3'),
-  
+
   blogPostView: null,
 
 	iframe: null,
-  
+
   showBlogPane: function() {
     this.blogPostView = Geniverse.BlogPostView.create();
 		this.set('iframe',SC.WebView.create({								//This is an empty iFrame used to make sure the InfoView will be on top of applets
@@ -57,35 +57,37 @@ Geniverse.blogPostController = SC.Controller.create(
     }
     this.blogPostView.append();
   },
-  
+
   hideBlogPane: function() {
     // save content in case in needs to be restored
     this.set('_savedTitle', this.get('title'));
     this.set('_savedContent1', this.get('content1'));
     this.set('_savedContent2', this.get('content2'));
     this.set('_savedContent3', this.get('content3'));
-    
+
     this.set('title', '');
     this.set('content1', '');
     this.set('content2', '');
     this.set('content3', '');
     this.blogPostView.remove();
-		this.get('iframe').parentView.removeChild(this.get('iframe'));
+    if (this.get('iframe') && this.get('iframe').get('parentView')) {
+      this.get('iframe').get('parentView').removeChild(this.get('iframe'));
+    }
   },
-  
+
   restoreBlogPost: function() {
     this.set('title', this.get('_savedTitle'));
     this.set('content1', this.get('_savedContent1'));
     this.set('content2', this.get('_savedContent2'));
     this.set('content3', this.get('_savedContent3'));
   },
-  
+
   _savedTitle: "",
-  
+
   _savedContent1: "",
-  
+
   _savedContent2: "",
-  
+
   _savedContent3: ""
 
 }) ;
