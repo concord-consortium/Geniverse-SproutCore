afd38f72ba0c4a842345deb5fc06a08b07cf20b9
Aaron Unger <aaron.unger@gmail.com>
2012-09-20 10:58:41 -0700

    Comment out some console logs

--- apps/lab/routes.js
+++ apps/lab/routes.js
@@ -115,12 +115,12 @@ Lab.routes = SC.Object.create({
     
     // Don't bother with SC.logger.log(). console.log() logs an inspectable object instead of forcibly converting
     // all its arguments to strings.
-    console.log("  pageOwner: %s", pageOwner && pageOwner.toString(), pageOwner);
-    console.log("  routeParams: %s", routeParams && routeParams.toString(), routeParams);
-    console.log("  pageName: %s", pageName);
-    console.log("  paneName: %s", paneName);
-    console.log("  page: %s", page && page.toString(), page);
-    console.log("  pane: %s", pane && pane.toString(), pane);
+    // console.log("  pageOwner: %s", pageOwner && pageOwner.toString(), pageOwner);
+    // console.log("  routeParams: %s", routeParams && routeParams.toString(), routeParams);
+    // console.log("  pageName: %s", pageName);
+    // console.log("  paneName: %s", paneName);
+    // console.log("  page: %s", page && page.toString(), page);
+    // console.log("  pane: %s", pane && pane.toString(), pane);
     console.log("END Lab.routes.gotoRoute()");
   }
 
