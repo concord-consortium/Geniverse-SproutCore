586e0ad408661338bfd1338c4cdbfd6b54fa9ad7
Aaron Unger <aaron.unger@gmail.com>
2012-10-31 12:03:16 -0700

    Resize unlockable notifications to fit more text

--- frameworks/geniverse/views/unlockable_notification.js
+++ frameworks/geniverse/views/unlockable_notification.js
@@ -5,7 +5,7 @@
 /*globals Geniverse */
 
 Geniverse.UnlockableNotificationView = SC.PanelPane.extend({
-  layout: { right: 30, top: 50, width: 200, height: 60 },
+  layout: { right: 30, top: 50, width: 200, height: 68 },
   isModal: NO,
   message: "",
   contentView: SC.View.design({
@@ -13,7 +13,7 @@ Geniverse.UnlockableNotificationView = SC.PanelPane.extend({
     childViews: 'title message closeButton'.w(),
     messageBinding: '*parentView.message',
     title: SC.LabelView.design({
-      layout: { top: 5, left: 5, width: 190, height: 24 },
+      layout: { top: 5, left: 5, width: 190, height: 20 },
       classNames: ['notification-title'],
       fontWeight: SC.BOLD_WEIGHT,
       value: "You've unlocked:"
@@ -26,7 +26,7 @@ Geniverse.UnlockableNotificationView = SC.PanelPane.extend({
       }
     }),
     message: SC.LabelView.design({
-      layout: { bottom: 5, left: 5, width: 190, height: 24 },
+      layout: { top: 25, left: 5, width: 190, height: 36 },
       classNames: ['notification-message'],
       valueBinding: '*parentView.message'
     })
