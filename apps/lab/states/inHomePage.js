4a51ffd3da1ac9a7d3942f85af42e99f1d5c26e6
Aaron Unger <aaron.unger@gmail.com>
2012-11-01 10:45:26 -0700

    Fix page title for the home/office page

--- apps/lab/states/inHomePage.js
+++ apps/lab/states/inHomePage.js
@@ -7,6 +7,13 @@
 Lab.inHomePage = Ki.State.extend({
 
   enterState: function() {
+    if (Geniverse.activityController.get('title')) {
+      Geniverse.activityController.set('title', "Office");
+    } else {
+      Geniverse.activityController.set('content', Geniverse.store.createRecord(Geniverse.Activity, {
+        title: "Office"
+      }));
+    }
     Lab.routes.gotoLabRoute({pageName: 'mainPage'});
   },
 
