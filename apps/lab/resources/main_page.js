d191c088df089b93e963929a6aa4b28ea3b79480
Aaron Unger <aaron.unger@gmail.com>
2012-11-05 09:13:15 -0800

    Add door rollover in office for logging out

--- apps/lab/resources/main_page.js
+++ apps/lab/resources/main_page.js
@@ -22,7 +22,7 @@ Lab.mainPage = SC.Page.design({
     mainAppView: SC.View.design({
       layout: { top: 57, centerX: 0, width: 1890, height: 890},
       classNames: ['mainscreen'],
-      childViews: 'caselogButtonView journalButtonView'.w(),
+      childViews: 'caselogButtonView journalButtonView doorButtonView'.w(),
 
       caselogButtonView: SC.View.design(Geniverse.SimpleButton, {
         layerId: 'caselog',
@@ -46,6 +46,18 @@ Lab.mainPage = SC.Page.design({
         target: 'Lab.journalController',
         action: 'openWindow',
         value: static_url('mainscreen-journal-highlighted')
+      }),
+
+      doorButtonView: SC.View.design(Geniverse.SimpleButton, {
+        layerId: 'door',
+        tagName: 'a',
+        hasHover: YES,
+        layout: { top: 23, centerX: 403, width: 302, height: 485 },
+        alt: 'Log out',
+        toolTip: 'Log out',
+        target: 'Lab.statechart',
+        action: 'logOut',
+        value: static_url('mainscreen-door-highlighted')
       }),
 
       focusMainFrom: function(pane) {
