093bcafb9a95ab5001a2dcd7593fd297f832a195
Aaron Unger <aaron.unger@gmail.com>
2012-11-06 16:23:01 -0800

    Smaller versions of the background images

--- apps/lab/views/background.js
+++
@@ -1,5 +0,0 @@
-Lab.BackgroundView = SC.ImageView.extend(
-/** @scope Lab.BackgroundView.prototype */ {
-  value: static_url('lab_background.png'),
-  layout: {width: 1890, height: 890, top: 0, centerX: 0}
-});
