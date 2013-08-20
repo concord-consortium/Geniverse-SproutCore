d23a625b34a4be83804ba49dbecc06ae441a9dcc
Sam Fentress <sfentress@concord.org>
2013-08-20 14:23:00 -0400

    Tweaks to caselog titles.
    [#54124735]

--- apps/lab/cases.js
+++ apps/lab/cases.js
@@ -171,10 +171,10 @@
               title: "Challenge 2: Receeding Armor",
               href: "#case13/challenge2"
             }, {
-              title: "Challenge 3: Disarming, Dislegging",
+              title: "Challenge 3: Disarming... and Dislegging",
               href: "#case13/challenge3"
             }, {
-              title: "Challenge 2: Going, Going...",
+              title: "Challenge 4: Going, Going...",
               href: "#case13/challenge4"
             }
           ]
