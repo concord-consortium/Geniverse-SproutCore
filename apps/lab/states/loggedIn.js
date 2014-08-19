concord-consortium/Geniverse-SproutCore

commit ebed52ca977924116ea339bb396b6e1f5e659691

Move duplicated QTip functions to loggedIn state
[#65197072]

apps/lab/states/loggedIn.js
@@ -112,6 +112,113 @@ Lab.loggedIn = Ki.State.extend({
       // this notifies app of statechange so is not as good...
       window.location.hash = '';
     }
+  },
+
+  // *** tooltip-related functions ***
+    // Shows a tooltip on a jquery element, with a given text and options.
+  // The options hash can specify a target (topLeft, leftMiddle, etc), a tooltip
+  // (same values but for the tail of the tooltip), a maxWidth and a hideAction.
+  // For convenience, if the $elem has title text then the tooltip will use that,
+  // and if it has the classes 'hint-target-*' or 'hint-tooltip-*' it will pass
+  // the * values as the appropriate options.
+  showTooltip: function($elem, text, options) {
+
+    if (!$elem.attr("alt")) {
+      $elem.attr("alt", $elem.attr("title"));
+    }
+
+    var backdrop, config, style, classes, elemClass, i,
+        opts      = options || {};
+        target    = opts.target     || "leftMiddle",
+        tooltip   = opts.tooltip    || "rightMiddle",
+        maxWidth  = opts.maxWidth   || 280,
+        text      = text || $elem.attr("alt"),
+        dark      = false,
+        showOnReady = opts.showOnReady == null ? true : opts.showOnReady;
+
+    if (!text) {
+      return;
+    }
+
+    classes = $elem.attr('class').split(/\s+/);
+    for (i = 0; i < classes.length; i++) {
+      elemClass = classes[i];
+      if (/hint-target-(.*)/.exec(elemClass)) {
+        target = /hint-target-(.*)/.exec(elemClass)[1];
+      } else if (/hint-tooltip-(.*)/.exec(elemClass)) {
+        tooltip = /hint-tooltip-(.*)/.exec(elemClass)[1];
+      } else if (/hint-dark/.exec(elemClass)) {
+        dark = true;
+      }
+    }
+
+    style = {
+      width: {
+        min: 240
+      },
+      padding: '14px',
+      border: {
+        width: 0,
+        radius: 2,
+        color: '#FFF'
+      },
+      color: '#FFF'
+    };
+    style.tip = tooltip;
+    config = {
+      content: {
+        title: {
+          text: ''
+        },
+        text: text
+      },
+      position: {
+        corner: {
+          target: target,
+          tooltip: tooltip
+        }
+      },
+      show: {
+        ready: showOnReady,
+        solo: false,
+        effect: { type: 'fade', length: 800 }
+      },
+      hide: {
+        effect: { type: 'fade' }
+      },
+      style: style,
+      api: {
+        onRender: function() {
+          this.elements.tooltip.click(this.hide);
+        }
+      }
+    };
+    if (opts.hideAction != null) {
+      config.api.onHide = opts.hideAction;
+    }
+
+    $elem.attr('title', '');  // rm title attribute so we don't see it as well
+
+    return $elem.qtip(config);
+  },
+
+  showAllTooltips: function(elemClass) {
+    // Rm any created tooltips
+    $(".qtip").remove();
+
+    var selection = $("."+elemClass),
+        self = this;
+    selection.each(function(){
+      self.showTooltip($(this));
+    });
+  },
+
+  registerAllTooltips: function(elemClass) {
+    var selection = $("."+elemClass),
+        self = this;
+    selection.each(function(){
+      self.showTooltip($(this), null, {showOnReady: false});
+    });
   }
 
 });

apps/lab/states/inActivity.js
@@ -216,112 +216,6 @@ Lab.inActivity = Ki.State.extend({
     this.gotoState('inEndingHub');
   },
 
-  QTipStyle: {
-    width: {
-      max: 350
-    },
-    padding: '14px',
-    border: {
-      width: 0,
-      radius: 2,
-      color: '#FFFFFF'
-    },
-    color: '#FFFFFF'
-  },
-
-  // Shows a tooltip on a jquery element, with a given text and options.
-  // The options hash can specify a target (topLeft, leftMiddle, etc), a tooltip
-  // (same values but for the tail of the tooltip), a maxWidth and a hideAction.
-  // For convenience, if the $elem has title text then the tooltip will use that,
-  // and if it has the classes 'hint-target-*' or 'hint-tooltip-*' it will pass
-  // the * values as the appropriate options.
-  showTooltip: function($elem, text, options) {
-    if (!Geniverse.activityController.getPath('content.showTooltips')) return;
-
-    if (!$elem.attr("alt")) {
-      $elem.attr("alt", $elem.attr("title"));
-    }
-
-    var backdrop, config, style, classes, elemClass, i,
-        opts      = options || {};
-        target    = opts.target     || "leftMiddle",
-        tooltip   = opts.tooltip    || "rightMiddle",
-        maxWidth  = opts.maxWidth   || 280,
-        text      = text || $elem.attr("alt"),
-        dark      = false;
-
-    if (!text) {
-      return;
-    }
-
-    classes = $elem.attr('class').split(/\s+/);
-    for (i = 0; i < classes.length; i++) {
-      elemClass = classes[i];
-      if (/hint-target-(.*)/.exec(elemClass)) {
-        target = /hint-target-(.*)/.exec(elemClass)[1];
-      } else if (/hint-tooltip-(.*)/.exec(elemClass)) {
-        tooltip = /hint-tooltip-(.*)/.exec(elemClass)[1];
-      } else if (/hint-dark/.exec(elemClass)) {
-        dark = true;
-      }
-    }
-
-    style = SC.clone(this.QTipStyle, true);
-    if (dark) {
-      style.border.color = "#777";
-    }
-    style.tip = tooltip;
-    style.width = {
-      max: maxWidth
-    };
-    config = {
-      content: {
-        title: {
-          text: ''
-        },
-        text: text
-      },
-      position: {
-        corner: {
-          target: target,
-          tooltip: tooltip
-        }
-      },
-      show: {
-        ready: true,
-        solo: false,
-        effect: { type: 'fade', length: 800 }
-      },
-      hide: {
-        effect: { type: 'fade' }
-      },
-      style: style,
-      api: {
-        onRender: function() {
-          this.elements.tooltip.click(this.hide);
-        }
-      }
-    };
-    if (opts.hideAction != null) {
-      config.api.onHide = opts.hideAction;
-    }
-
-    $elem.attr('title', '');  // rm title attribute so we don't see it as well
-
-    return $elem.qtip(config);
-  },
-
-  showAllTooltips: function(elemClass) {
-    // Rm any created tooltips
-    $(".qtip").remove();
-
-    var selection = $("."+elemClass),
-        self = this;
-    selection.each(function(){
-      self.showTooltip($(this));
-    });
-  },
-
   // show secondary hints, if there are any
   meiosisAnimationCompleted: function(mode) {
     if (mode !== "offspring") {

apps/lab/states/inHomePage.js
@@ -24,113 +24,7 @@ Lab.inHomePage = Ki.State.extend({
       setTimeout( function() {
         $(".qtip").fadeOut();
       },3500);
-    },1500);
-  },
-
-  // Shows a tooltip on a jquery element, with a given text and options.
-  // The options hash can specify a target (topLeft, leftMiddle, etc), a tooltip
-  // (same values but for the tail of the tooltip), a maxWidth and a hideAction.
-  // For convenience, if the $elem has title text then the tooltip will use that,
-  // and if it has the classes 'hint-target-*' or 'hint-tooltip-*' it will pass
-  // the * values as the appropriate options.
-  showTooltip: function($elem, text, options) {
-
-    if (!$elem.attr("alt")) {
-      $elem.attr("alt", $elem.attr("title"));
-    }
-
-    var backdrop, config, style, classes, elemClass, i,
-        opts      = options || {};
-        target    = opts.target     || "leftMiddle",
-        tooltip   = opts.tooltip    || "rightMiddle",
-        maxWidth  = opts.maxWidth   || 280,
-        text      = text || $elem.attr("alt"),
-        dark      = false,
-        showOnReady = opts.showOnReady == null ? true : opts.showOnReady;
-
-    if (!text) {
-      return;
-    }
-
-    classes = $elem.attr('class').split(/\s+/);
-    for (i = 0; i < classes.length; i++) {
-      elemClass = classes[i];
-      if (/hint-target-(.*)/.exec(elemClass)) {
-        target = /hint-target-(.*)/.exec(elemClass)[1];
-      } else if (/hint-tooltip-(.*)/.exec(elemClass)) {
-        tooltip = /hint-tooltip-(.*)/.exec(elemClass)[1];
-      } else if (/hint-dark/.exec(elemClass)) {
-        dark = true;
-      }
-    }
-
-    style = {
-      width: {
-        min: 240
-      },
-      padding: '14px',
-      border: {
-        width: 0,
-        radius: 2,
-        color: '#FFF'
-      },
-      color: '#FFF'
-    };
-    style.tip = tooltip;
-    config = {
-      content: {
-        title: {
-          text: ''
-        },
-        text: text
-      },
-      position: {
-        corner: {
-          target: target,
-          tooltip: tooltip
-        }
-      },
-      show: {
-        ready: showOnReady,
-        solo: false,
-        effect: { type: 'fade', length: 800 }
-      },
-      hide: {
-        effect: { type: 'fade' }
-      },
-      style: style,
-      api: {
-        onRender: function() {
-          this.elements.tooltip.click(this.hide);
-        }
-      }
-    };
-    if (opts.hideAction != null) {
-      config.api.onHide = opts.hideAction;
-    }
-
-    $elem.attr('title', '');  // rm title attribute so we don't see it as well
-
-    return $elem.qtip(config);
-  },
-
-  showAllTooltips: function(elemClass) {
-    // Rm any created tooltips
-    $(".qtip").remove();
-
-    var selection = $("."+elemClass),
-        self = this;
-    selection.each(function(){
-      self.showTooltip($(this));
-    });
-  },
-
-  registerAllTooltips: function(elemClass) {
-    var selection = $("."+elemClass),
-        self = this;
-    selection.each(function(){
-      self.showTooltip($(this), null, {showOnReady: false});
-    });
+    },500);
   },
 
   exitState: function() {

commit cdd73d86917eb422cebb2aa1afb2fb9e6e2b44cf

Check showTooltips (broken by ebed52ca97)
[#68885506]

apps/lab/states/loggedIn.js
@@ -128,6 +128,8 @@ Lab.loggedIn = Ki.State.extend({
   // and if it has the classes 'hint-target-*' or 'hint-tooltip-*' it will pass
   // the * values as the appropriate options.
   showTooltip: function($elem, text, options) {
+    if (!Geniverse.activityController.getPath('content.showTooltips') &&
+        Geniverse.activityController.getPath('content.title') !== "Office") return;
 
     if (!$elem.attr("alt")) {
       $elem.attr("alt", $elem.attr("title"));