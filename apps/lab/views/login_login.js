e0d04df807e3e2751cecd975764c47cfd4fda9ea
Sam Fentress <sfentress@concord.org>
2016-06-23 15:10:58 -0400

    Add Demo site link to login page

--- apps/lab/views/login_login.js
+++ apps/lab/views/login_login.js
@@ -13,7 +13,7 @@
 Lab.LoginLoginView = SC.View.extend({
      layout: {left: 0, top: 0, width: 400, height: 100},
 
-     childViews: 'logo loginButtonView welcomeView usernameLabel usernameView passwordLabel passwordView registerView'.w(),
+     childViews: 'logo loginButtonView welcomeView usernameLabel usernameView passwordLabel passwordView registerView demoLinkView'.w(),
 
      logo: SC.ImageView.design({
        layout: {top: 0, left: 11, width: 188, height: 84 },
@@ -33,9 +33,15 @@ Lab.LoginLoginView = SC.View.extend({
      }),
 
      registerView: SC.LabelView.design({
-       layout: { bottom: 8, height: 20, left: 0, width: 210 },
+       layout: { bottom: 77, height: 20, left: 0, width: 210 },
        escapeHTML: false,
        value: "Don't have a login? <a style='text-decoration:none; color:#2a777b;' href='/portal'>Register now <span style='color:#eb8723;'>&raquo;</span></a>"
+     }),
+
+     demoLinkView: SC.LabelView.design({
+       layout: { bottom: 11, height: 45, left: 0, width: 210 },
+       escapeHTML: false,
+       value: "Want to try the game without saving data? <a style='text-decoration:none; color:#2a777b;' href='http://demo.geniverse.concord.org'>Go to the Demo Site <span style='color:#eb8723;'>&raquo;</span></a>"
      }),
 
 
